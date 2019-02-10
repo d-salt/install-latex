f5b29d8b9e5a8dd8db916a6062bc3d59c134b1bd
Daiki Shiozawa <d-salt@users.noreply.github.com>
2019-02-10 15:08:02 +0900

    add linux installer

---
+++ etc/init/linux/packages.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -Cue
+
+: "Install LaTeX and packages"  && {
+    echo 'Download TeX Live...' 
+    if is_exists "curl"; then
+      curl http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+    elif is_exists "wget"; then
+      wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+    else
+      echo "error: required: curl or wget"
+      exit 1
+    fi
+    tar xvf install-tl-unx.tar.gz
+    echo 'Install TeX Live and packages...' 
+    cd install-tl*
+    sudo ./install-tl --repository http://mirror.ctan.org/systems/texlive/tlnet/
+    ln -snvf /usr/local/texlive/????/bin/*/tlmgr /usr/local/
+    
+    sudo tlmgr update --self --all
+    sudo tlmgr paper a4
+    sudo tlmgr install collection-langjapanese
+    sudo tlmgr install latexmk
+}
+
+
+: "Note" && {
+    echo ''
+    echo 'Note:'
+    echo "    to see realtime preview exact: latexmk -pvc"
+    echo "    to enable realtime preview on skim, check"
+    echo "    preference -> Sync -> Check for file changes"
+}
+
+exit 0
+
